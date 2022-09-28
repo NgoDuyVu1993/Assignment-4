f9794eee88e4fbc1891d71bebad28bdbd16b8131
NgoDuyVu1993 <duyvu.obzervr@gmail.com>
2022-09-28 18:51:37 +1000

    second test

---
+++ udagram/set_env.sh
@@ -0,0 +1,13 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres_123
+export POSTGRES_HOST=database-1.chrsmwzfuri8.ap-southeast-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export PORT=8080
+export PORT_DB=5432
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=ap-southeast-2
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
