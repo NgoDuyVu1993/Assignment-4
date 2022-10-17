d0d99a6c0c7c6cf80a2ce298364bbfb29d41646b
NgoDuyVu1993 <114485635+NgoDuyVu1993@users.noreply.github.com>
2022-10-17 23:05:55 +0530

    Fixed compilation error

--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -25,7 +25,7 @@ export function getGetSignedUrl(key: string): string {
 }
 
 // Generates an AWS signed URL for uploading objects
-export function getPutSignedUrl(key: string, callback: any): string {
+export function getPutSignedUrl(key: string, callback: any) {
   const signedUrlExpireSeconds = 60 * 5;
   const s3 = new AWS.S3({
   
