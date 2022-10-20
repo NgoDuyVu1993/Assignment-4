5648d938f8e56715b50dbde25a6d1152b28965fa
NgoDuyVu1993 <114485635+NgoDuyVu1993@users.noreply.github.com>
2022-10-20 16:29:30 +0530

    got url obj

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -56,9 +56,9 @@ export class ApiService {
 
   async upload(endpoint: string, file: File, payload: any): Promise<any> {
     const signed_url = (await this.get(`${endpoint}/signed-url/${file.name}`));
-    console.log(signed_url);
+    console.log(signed_url.url);
     const headers = new HttpHeaders({'Content-Type': file.type});
-    const req = new HttpRequest( 'PUT', signed_url, file,
+    const req = new HttpRequest( 'PUT', signed_url.url, file,
                                   {
                                     headers: headers,
                                     reportProgress: true, // track progress
